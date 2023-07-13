4254d88b6188ffb6fef560bb19722596f94d88ad
Kutaiba Bahash <93405663+kutaiba98@users.noreply.github.com>
2023-07-13 16:54:36 +0300

    Add files via upload

---
+++ tests/example/tests.test.js
@@ -0,0 +1,36 @@
+//הוספת מוצר חדש למאגר המוצרים:
+describe('Add new product', function() {
+    it('should add a new product to the product repository', function() {
+      // Test code for adding a new product
+      // ...
+      expect(/* Add your expectation here */).toBeTruthy();
+    });
+  });
+
+  //הוספת מוצר לעגלה:
+  describe('Add product to cart', function() {
+    it('should add a product to the shopping cart', function() {
+      // Test code for adding a product to the cart
+      // ...
+      expect(/* Add your expectation here */).toBeTruthy();
+    });
+  });
+
+  //הסרת מוצר מהעגלה:
+  describe('Remove product from cart', function() {
+    it('should remove a product from the shopping cart', function() {
+      // Test code for removing a product from the cart
+      // ...
+      expect(/* Add your expectation here */).toBeTruthy();
+    });
+  });
+
+  //תהליך רכישה כללי:
+  describe('General purchase process', function() {
+    it('should complete the general purchase process', function() {
+      // Test code for the general purchase process
+      // ...
+      expect(/* Add your expectation here */).toBeTruthy();
+    });
+  });
+  
